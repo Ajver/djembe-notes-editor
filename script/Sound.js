3a62b0638e881c04cc8c6e5489d6672f93cd36a7
Dominik <zawlockidominik@gmail.com>
2023-09-13 15:30:56 +0200

    Playing/Pausing rythm

---
+++ script/Sound.js
@@ -0,0 +1,99 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const context = new AudioContext();
+
+// Thanx to https://stackoverflow.com/questions/25654558/html5-js-play-same-sound-multiple-times-at-the-same-time/76916935#76916935?newreg=0c69169e05424502b2da5acc98455370
+class Sound {
+    url = '';
+    buffer = null;
+    sources = [];
+
+    constructor(url) {
+        this.url = url;
+    }
+
+    load() {
+        if (!this.url) return Promise.reject(new Error('Missing or invalid URL: ', this.url));
+
+        if (this.buffer) return Promise.resolve(this.buffer);
+
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+
+            request.open('GET', this.url, true);
+            request.responseType = 'arraybuffer';
+
+            // Decode asynchronously:
+
+            request.onload = () => {
+                context.decodeAudioData(request.response, (buffer) => {
+                    if (!buffer) {
+                        console.log(`Sound decoding error: ${ this.url }`);
+
+                        reject(new Error(`Sound decoding error: ${ this.url }`));
+
+                        return;
+                    }
+
+                    this.buffer = buffer;
+
+                    resolve(buffer);
+                });
+            };
+
+            request.onerror = (err) => {
+                console.log('Sound XMLHttpRequest error:', err);
+
+                reject(err);
+            };
+
+            request.send();
+        });
+    }
+
+    play(volume = 1, time = 0) {
+        if (!this.buffer) return;
+
+        // Create a new sound source and assign it the loaded sound's buffer:
+
+        const source = context.createBufferSource();
+
+        source.buffer = this.buffer;
+
+        // Keep track of all sources created, and stop tracking them once they finish playing:
+
+        this.sources.push(source);
+
+        source.onended = () => {
+            source.stop(0);
+
+            const idx = Array.prototype.indexOf.call(this.sources, source)
+            this.sources.splice(idx, 1);
+        };
+
+        // Create a gain node with the desired volume:
+
+        const gainNode = context.createGain();
+
+        gainNode.gain.value = volume;
+
+        // Connect nodes:
+
+        source.connect(gainNode).connect(context.destination);
+
+        // Start playing at the desired time:
+
+        source.start(time);
+    }
+
+    stop() {
+        // Stop any sources still playing:
+
+        this.sources.forEach((source) => {
+            source.stop(0);
+        });
+
+        this.sources = [];
+    }
+
+}
---
+++ script/play.js
@@ -0,0 +1,100 @@
+
+let isPlayingRythm = false
+let nextTimeoutId = 0
+
+const sounds = {
+    "bass": new Sound("sound/Djembe_bass.wav"),
+    "tone": new Sound("sound/Djembe_tone.wav"),
+    "slap": new Sound("sound/Djembe_slap.wav"),
+}
+
+Object.values(sounds).forEach(sound => {
+    sound.load()
+})
+
+
+const onRythmPlayEnd = () => {
+    document.querySelector("#play-btn").innerHTML = "PLAY"
+}
+
+document.querySelector("#play-btn").addEventListener("click", () => {
+    if (isPlayingRythm) {
+        isPlayingRythm = false
+        onRythmPlayEnd()
+        return
+    }
+
+    document.querySelector("#play-btn").innerHTML = "PAUSE"
+
+    // Delays are in millis
+    const singleBitDelay = (60_000.0 / rythmTempo) / notesInBar
+    const doubleBitNoteDelay = singleBitDelay / 2
+    const tripletBitNoteDelay = (singleBitDelay * 2) / 3
+
+    console.log(singleBitDelay, doubleBitNoteDelay, tripletBitNoteDelay)
+
+    // Every note is a pointer to the sound and delay in millis to the next one
+    let notesToPlay = []
+
+    document.querySelectorAll(".bit").forEach((bitBtn) => {
+        if (bitBtn.classList.contains("hidden")) {
+            // Skip hidden bits
+            return
+        }
+
+        const bitType = bitBtn.getAttribute("bit-type")
+
+        bitBtn.querySelectorAll("img").forEach((noteBtn) => {
+            const note = noteBtn.getAttribute("note")
+            const sound = sounds[note]
+
+            let delay = singleBitDelay
+
+            if (bitType === "double") {
+                delay = doubleBitNoteDelay
+            }else if (bitType === "triplet") {
+                delay = tripletBitNoteDelay
+            }
+
+            console.log(note, sound, delay)
+
+            notesToPlay.push({
+                sound: sound,
+                delay: delay,
+            })
+        })
+    })
+
+    console.log(notesToPlay)
+
+    if (notesToPlay.length == 0) {
+        console.log("No notes to play")
+        onRythmPlayEnd
+        return
+    }
+    
+    const playAndContinue = (playedNoteIdx) => {
+        if (!isPlayingRythm) {
+            return
+        }
+
+        const {sound, delay} = notesToPlay[playedNoteIdx]
+        
+        if (sound) {
+            sound.play()
+        }
+
+        if (playedNoteIdx + 1 >= notesToPlay.length) {
+            // The rythm has ended
+            onRythmPlayEnd()
+            return
+        }
+
+        nextTimeoutId = window.setTimeout(() => {
+            playAndContinue(playedNoteIdx + 1)
+        }, delay)
+    }
+
+    isPlayingRythm = true
+    playAndContinue(0)
+})
