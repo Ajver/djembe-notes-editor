7609a34bb9cd3e8a70ef251d958812c3a9b9ad66
Dominik <zawlockidominik@gmail.com>
2023-10-15 22:30:32 +0200

    WIP: Input label container behavior

--- src/components/Editor/Sheet/InputLabelContainer.jsx
+++ src/components/Editor/Sheet/InputLabelContainer.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./css/InputLabelContainer.css"
 
-export default function InputLabelContainer({className, text, onChange}) {
+export default function InputLabelContainer({className, text, editCallback}) {
+  const [visible, setVisible] = useState(false)
+  
+  function show() {
+    setVisible(true)
+    
+    // TODO: Deselect all notes
+    // Fixes bug when one can edit label with notes selected
+  }
+
+  function handleFocus(event) {
+    event.target.select()
+  }
+
+  function onKeyDown(event) {
+    if (event.key === "Enter") {
+      event.target.blur()
+    } else if (event.key === "Escape") {
+        // Cancel editing
+        event.target.value = rhythmTitle
+        event.target.blur()
+    }
+  }
+
+  function onBlur(event) {
+    setVisible(false)
+    editCallback(input.value)
+    // if (newContent === undefined) {
+    //     label.innerHTML = input.value
+    // }else {
+    //     // Custom input content
+    //     label.innerHTML = newContent
+    // }
+  }
+
   return (
-    <div className={"input-label-container " + className}>
+    <div 
+    className={"input-label-container " + className} 
+    onClick={show}
+    onKeyDown={onKeyDown}
+    >
       <p>{text}</p>
-      <input type="text" />
+      <input 
+        type="text" 
+        value={text}
+        className={visible ? "visible" : ""} 
+        onChange={event => onChange(event.target.value)}
+        onFocus={handleFocus}
+        onBlur={onBlur}
+      />
     </div>
   )
 }
