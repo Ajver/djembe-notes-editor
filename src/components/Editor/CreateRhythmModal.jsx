9c6438d45886666e6aa01603ae6b0751a5f74fee
Dominik <zawlockidominik@gmail.com>
2023-10-24 01:06:22 +0200

    Add preset buttons to Create rhythm modal

--- src/components/Editor/CreateRhythmModal.jsx
+++ src/components/Editor/CreateRhythmModal.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ModalWrapper from "./ModalWrapper";
 import { useDispatch, useSelector } from "react-redux";
 import { setCreateRhythmModalVisibility } from "../../Redux/modalsSlice";
 import './css/CreateRhythmModal.css';
 
 export default function CreateRhythmModal() {
+  const [parallelInstruments, setParallelInstruments] = useState(1)
+
   const isVisible = useSelector(store => store.modals.createRhythmModalVisible)
   const dispatch = useDispatch()
 
@@ -18,11 +20,34 @@ export default function CreateRhythmModal() {
         <h2>Creating new rhythm</h2>
         <p><span className="warning">Warning!</span> The previous rhythm will be deleted! If you don't want to lose it, remember to export it first.</p>
         <div className="preset-selection-container">
-          {/* Add your radio buttons here */}
+          <input type="radio" className="preset" name="preset-selection-btn" id="preset-4-4" value="4-4" defaultChecked />
+          <label htmlFor="preset-4-4" title="4 beats in bar, 4 parts in each beat">
+              4/4
+          </label>
+          <input type="radio" className="preset" name="preset-selection-btn" id="preset-3-4" value="3-4" />
+          <label htmlFor="preset-3-4" title="3 beats in bar, 4 parts in each beat">
+              3/4
+          </label>
+          <input type="radio" className="preset" name="preset-selection-btn" id="preset-12-8" value="12-8" />
+          <label htmlFor="preset-12-8" title="4 beats in bar, 3 parts in each beat">
+              12/8
+          </label>
+          <input type="radio" className="preset" name="preset-selection-btn" id="preset-9-8" value="9-8" />
+          <label htmlFor="preset-9-8" title="3 beats in bar, 3 parts in each beat">
+              9/8
+          </label>
         </div>
         <div className="bars-in-full-score-section">
           <p title="How many rhythms will be played together">Parallel instruments:</p>
-          <input type="number" className="bars-in-full-score-input" min="1" max="10" value="1" title="How many rhythms will be played together" />
+          <input 
+            type="number" 
+            className="bars-in-full-score-input" 
+            min="1" 
+            max="10" 
+            value={parallelInstruments} 
+            onChange={e => setParallelInstruments(e.target.value)}
+            title="How many rhythms will be played together" 
+          />
         </div>
         <div className="buttons-section">
           <button className="cancel" onClick={hide}>CANCEL</button>
