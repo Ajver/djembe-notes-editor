713234dcddd8e86e832af3caf83cb7ac18b43f7c
Dominik <zawlockidominik@gmail.com>
2023-10-24 02:21:37 +0200

    Rename createBarBtn -> injectBarBtn

--- src/components/Editor/Sheet/Bar.jsx
+++ src/components/Editor/Sheet/Bar.jsx
@@ -3,14 +3,14 @@ import BeatsColumn from "./BeatsColumn"
 import "./css/Bar.css"
 
 export default function Bar({beats}) {
-  function createBarAfter() {
-    console.log("Creating bar after this one: ", beats)
+  function injectBarAfter() {
+    console.log("Injecting bar after this one: ", beats)
   }
 
   return (
     <div className="bar">
       {beats.map(beatIdx => <BeatsColumn key={beatIdx} beatIdx={beatIdx}/>)}
-      <div className="create-bar-btn editor-only" onClick={createBarAfter}></div>
+      <div className="inject-bar-btn editor-only" onClick={injectBarAfter}></div>
     </div>
   )
 }
