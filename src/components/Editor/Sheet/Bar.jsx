d81c99299963dc7b399287a9522b41ac0c708c64
Dominik <zawlockidominik@gmail.com>
2023-10-24 02:20:34 +0200

    Add createBarBtn to every bar

--- src/components/Editor/Sheet/Bar.jsx
+++ src/components/Editor/Sheet/Bar.jsx
@@ -3,9 +3,14 @@ import BeatsColumn from "./BeatsColumn"
 import "./css/Bar.css"
 
 export default function Bar({beats}) {
+  function createBarAfter() {
+    console.log("Creating bar after this one: ", beats)
+  }
+
   return (
     <div className="bar">
       {beats.map(beatIdx => <BeatsColumn key={beatIdx} beatIdx={beatIdx}/>)}
+      <div className="create-bar-btn editor-only" onClick={createBarAfter}></div>
     </div>
   )
 }
