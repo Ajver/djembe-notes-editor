bed744a9158fe33d433f970bf65465391342c689
Dominik <zawlockidominik@gmail.com>
2023-10-26 20:24:12 +0200

    Importing rhythm by Import btn

--- src/components/Editor/RhythmManagementPanel.jsx
+++ src/components/Editor/RhythmManagementPanel.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import "./css/RhythmManagementPanel.css"
 import { useDispatch } from "react-redux"
 import { setCreateRhythmModalVisibility, setExportModalVisibility } from "../../Redux/modalsSlice"
+import loadRhythmFromFile from "../../helpers/loadRhythmFromFile"
 
 export default function RhythmManagementPanel() {
   const dispatch = useDispatch()
 
-  function showExportModal() {
-    dispatch(setExportModalVisibility(true))
-  }
-
   function showCreateNewRhythmModal() {
     dispatch(setCreateRhythmModalVisibility(true))
+  }
+  
+  function importRhythm(event) {
+    console.log("IMPORTING: ", event)
+    loadRhythmFromFile(event.target.files[0], dispatch)
+  }
+
+  function showExportModal() {
+    dispatch(setExportModalVisibility(true))
   }
 
   return (
@@ -19,7 +25,7 @@ export default function RhythmManagementPanel() {
       <button onClick={showCreateNewRhythmModal}>Create new rhythm</button>
       <label className="label-button button">
             Import
-            <input type="file" id="import-btn" accept=".json" />
+            <input type="file" id="import-btn" accept=".json" onChange={importRhythm} />
         </label>
         <button onClick={showExportModal}>Export</button>
     </div>
