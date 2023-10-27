2fcf7c85f51efd91afb5021bdf215a6a3c28c8d5
Dominik <zawlockidominik@gmail.com>
2023-10-27 14:39:00 +0200

    Add '/' sign before 'assets' to all imgs in TipsPanel

--- src/components/Editor/TipsPanel.jsx
+++ src/components/Editor/TipsPanel.jsx
@@ -13,15 +13,15 @@ export default function TipsPanel() {
   return (
     <div className={"tips-panel " + (visible ? "" : "hidden")}>
       <div className="key-tips-container">
-          <div className="key-tip">`~<img src="assets/svg/dash.svg" /></div>
-          <div className="key-tip">1!<img src="assets/svg/bass.svg" /></div>
-          <div className="key-tip">2@<img src="assets/svg/tone.svg" /></div>
-          <div className="key-tip">3#<img src="assets/svg/cross.svg" /></div>
-          <div className="key-tip">4$<img src="assets/svg/ghost.svg" /></div>
-          <div className="key-tip">Shift + 1<p>single</p><img src="assets/svg/cross.svg" /></div>
-          <div className="key-tip double">Shift + 2<img src="assets/svg/cross.svg" /><img src="assets/svg/dash.svg" /></div>
-          <div className="key-tip triplet">Shift + 3<img src="assets/svg/cross.svg" /><img src="assets/svg/dash.svg" /><img src="assets/svg/dash.svg" /></div>
-          <div className="key-tip quartet">Shift + 4<img src="assets/svg/cross.svg" /><img src="assets/svg/dash.svg" /><img src="assets/svg/dash.svg" /><img src="assets/svg/dash.svg" /></div>
+          <div className="key-tip">`~<img src="/assets/svg/dash.svg" /></div>
+          <div className="key-tip">1!<img src="/assets/svg/bass.svg" /></div>
+          <div className="key-tip">2@<img src="/assets/svg/tone.svg" /></div>
+          <div className="key-tip">3#<img src="/assets/svg/cross.svg" /></div>
+          <div className="key-tip">4$<img src="/assets/svg/ghost.svg" /></div>
+          <div className="key-tip">Shift + 1<p>single</p><img src="/assets/svg/cross.svg" /></div>
+          <div className="key-tip double">Shift + 2<img src="/assets/svg/cross.svg" /><img src="/assets/svg/dash.svg" /></div>
+          <div className="key-tip triplet">Shift + 3<img src="/assets/svg/cross.svg" /><img src="/assets/svg/dash.svg" /><img src="/assets/svg/dash.svg" /></div>
+          <div className="key-tip quartet">Shift + 4<img src="/assets/svg/cross.svg" /><img src="/assets/svg/dash.svg" /><img src="/assets/svg/dash.svg" /><img src="/assets/svg/dash.svg" /></div>
           <div className="key-tip">Shift<p>multiselect</p></div>
       </div>
 
