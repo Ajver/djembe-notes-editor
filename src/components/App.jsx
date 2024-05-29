9583a73f635ca632dfe42b956e6fa3dc9107db9d
Dominik <zawlockidominik@gmail.com>
2024-05-29 20:23:26 +0200

    Use react-roter-dom for routing between pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react"
 import Editor from './Editor/Editor'
 import LoginPage from "./LoginPage/LoginPage"
 import PageNotFound from "./PageNotFound/PageNotFound"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  const pageName = window.location.pathname
-
-  const component = {
-    "/": <LoginPage />,
-    "/login": <LoginPage />,
-    "/editor": <Editor />,
-  }[pageName] || <PageNotFound />
-
-  return <>{component}</>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/editor" element={<Editor />}/>
+        <Route path="*" element={<PageNotFound />}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App
