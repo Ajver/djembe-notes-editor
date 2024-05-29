fb5d176f2d2b445696c3dcd5b7c78779dc88173d
Dominik <zawlockidominik@gmail.com>
2024-05-29 20:00:18 +0200

    Create basic flow for multiple pages and create PageNotFound

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
 import Editor from './Editor/Editor'
+import LoginPage from "./LoginPage/LoginPage"
+import PageNotFound from "./PageNotFound/PageNotFound"
 
 function App() {
-  return (
-    <>
-      <Editor />
-    </>
-  )
+  const [page, setPage] = useState("login")
+  const pageName = window.location.pathname
+  console.log("Location:", pageName)
+
+  let component
+
+  switch (pageName) {
+    case "/":
+      component = <LoginPage />
+      break
+    case "/login":
+      component = <LoginPage />
+      break
+    case "/editor":
+      component = <Editor />
+      break
+    default:
+      component = <PageNotFound />
+  }
+
+  return <>{component}</>
 }
 
 export default App
---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import "./css/PageNotFound.css"
+
+export default function PageNotFound() {
+  return (
+    <div className="page-not-found-container">
+      <h1>404</h1>
+      <p>Page not found</p>
+      <a href="/">Return to home page</a>
+    </div>
+  )
+}
