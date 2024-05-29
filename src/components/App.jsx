ffeaa05dc6073ff4d052e67d20c6de26aa5d1a05
Dominik <zawlockidominik@gmail.com>
2024-05-29 20:03:23 +0200

    Simplify choosing page component by using map, instead of switch statement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,13 @@ import LoginPage from "./LoginPage/LoginPage"
 import PageNotFound from "./PageNotFound/PageNotFound"
 
 function App() {
-  const [page, setPage] = useState("login")
   const pageName = window.location.pathname
-  console.log("Location:", pageName)
 
-  let component
-
-  switch (pageName) {
-    case "/":
-      component = <LoginPage />
-      break
-    case "/login":
-      component = <LoginPage />
-      break
-    case "/editor":
-      component = <Editor />
-      break
-    default:
-      component = <PageNotFound />
-  }
+  const component = {
+    "/": <LoginPage />,
+    "/login": <LoginPage />,
+    "/editor": <Editor />,
+  }[pageName] || <PageNotFound />
 
   return <>{component}</>
 }
