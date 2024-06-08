c24f0af98f46ca5f1a8182d7eebc87462b70476c
Dominik Zawlocki <zawlockidominik@gmail.com>
2024-06-08 21:56:31 +0200

    WIP: create hamburger menu for mobile

--- src/components/Editor/TopPanel.jsx
+++ src/components/Editor/TopPanel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import "./css/TopPanel.css"
 import { setCreateRhythmModalVisibility } from "../../Redux/modalsSlice"
@@ -8,7 +8,12 @@ import InputLabelContainer from "../Common/InputLabelContainer"
 
 export default function TopPanel() {
   const dispatch = useDispatch()
-  const rhythmTitle = useSelector(store => store.rhythm.title)
+  
+  const [toDesktop, setToDesktop] = useState(document.body.offsetWidth >= 830)
+
+  window.addEventListener("resize", () => {
+    setToDesktop(document.body.offsetWidth >= 830)
+  })
 
   function showCreateNewRhythmModal() {
     dispatch(setCreateRhythmModalVisibility(true))
@@ -24,8 +29,28 @@ export default function TopPanel() {
   }
 
   return (
+    <>
+      { toDesktop 
+      ? <DesktopTopPanel
+        showCreateNewRhythmModal={showCreateNewRhythmModal}
+        importRhythm={importRhythm}
+        onRhythmTitleEdited={onRhythmTitleEdited}
+      /> 
+      : <MobileTopPanel
+        showCreateNewRhythmModal={showCreateNewRhythmModal}
+        importRhythm={importRhythm}
+        onRhythmTitleEdited={onRhythmTitleEdited}
+      /> }
+    </>
+  )
+}
+
+function DesktopTopPanel({showCreateNewRhythmModal, importRhythm, onRhythmTitleEdited}) {
+  const rhythmTitle = useSelector(store => store.rhythm.title)
+
+  return (
     <div className="top-panel">
-      <div className="rhythms-collection">
+      <section className="rhythms-collection">
         <button className="icon-btn"><img src="/assets/svg/ui/library.svg" alt="Show collection" /></button>
         <button className="icon-btn" onClick={showCreateNewRhythmModal}>
           <img src="/assets/svg/ui/create-document.svg" alt="create rhythm" />
@@ -34,9 +59,9 @@ export default function TopPanel() {
           <img src="/assets/svg/ui/import.svg" alt="Import rhythm from file" />
           <input type="file" id="import-btn" accept=".json" onChange={importRhythm} />
         </label>
-      </div>
+      </section>
 
-      <div className="rhythm-management">
+      <section className="rhythm-management">
         <button className="icon-btn">
           <img src="/assets/svg/ui/settings.svg" alt="Rhythm settings" />
         </button>
@@ -45,9 +70,9 @@ export default function TopPanel() {
           text={rhythmTitle} 
           editCallback={onRhythmTitleEdited}
         />
-      </div>
+      </section>
 
-      <div className="sharing-section">
+      <section className="sharing-section">
         <button className="icon-btn">
           <img src="/assets/svg/ui/cloud-check.svg" alt="Rhythm saved in cloud" />
         </button>
@@ -57,8 +82,59 @@ export default function TopPanel() {
         <button className="icon-btn">
           <img src="/assets/svg/ui/user.svg" alt="Profile options" />
         </button>
-      </div>
+      </section>
     </div>
   )
+}
 
+function MobileTopPanel({showCreateNewRhythmModal, importRhythm, onRhythmTitleEdited}) {
+  const rhythmTitle = useSelector(store => store.rhythm.title)
+  const [isMenuVisible, setMenuVisible] = useState(false)
+
+  function toggleMenu() {
+    setMenuVisible(!isMenuVisible)
+  }
+
+  const menuVisibleClass = isMenuVisible ? "menu-visible" : ""
+
+  return (
+    <div className={"top-panel " + menuVisibleClass}>
+      <section className="top-bar">
+        <button className="icon-btn" onClick={toggleMenu}>
+          { 
+            isMenuVisible 
+            ? <img src="/assets/svg/ui/close.svg" alt="Hide main menu" />
+            : <img src="/assets/svg/ui/hamburger.svg" alt="Show main menu" />
+          }
+        </button>
+        <InputLabelContainer 
+          className="title-edit" 
+          text={rhythmTitle} 
+          editCallback={onRhythmTitleEdited}
+        />
+        <button className="icon-btn">
+          <img src="/assets/svg/ui/share.svg" alt="Share rhythm" />
+        </button>
+      </section>
+      <nav className="hamburger-menu">
+        <button className="icon-btn"><img src="/assets/svg/ui/library.svg" alt="Show collection" /></button>
+        <button className="icon-btn" onClick={showCreateNewRhythmModal}>
+          <img src="/assets/svg/ui/create-document.svg" alt="create rhythm" />
+        </button>
+        <label className="label-button button icon-btn" title="Import rhythm from file">
+          <img src="/assets/svg/ui/import.svg" alt="Import rhythm from file" />
+          <input type="file" id="import-btn" accept=".json" onChange={importRhythm} />
+        </label>
+        <button className="icon-btn">
+          <img src="/assets/svg/ui/settings.svg" alt="Rhythm settings" />
+        </button>
+        <button className="icon-btn">
+          <img src="/assets/svg/ui/cloud-check.svg" alt="Rhythm saved in cloud" />
+        </button>
+        <button className="icon-btn">
+          <img src="/assets/svg/ui/user.svg" alt="Profile options" />
+        </button>
+      </nav>
+    </div>
+  )
 }
