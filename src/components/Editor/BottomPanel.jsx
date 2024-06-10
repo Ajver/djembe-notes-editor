4aac86f51fc4b3bed5a3752ad323237f2b989a40
Dominik Zawlocki <zawlockidominik@gmail.com>
2024-06-10 09:12:23 +0200

    (WIP): Create UI for bottom panel on mobile

--- src/components/Editor/BottomPanel.jsx
+++ src/components/Editor/BottomPanel.jsx
@@ -23,20 +23,41 @@ export default function BottomPanel() {
   return (
     <>
       <TipsPanel visible={tipsVisible} toggleVisibility={toggleTipsPanel} />
-      <div className="bottom-panel">
-        <div>
+      <section className="control-panel">
+        <section>
+          {/* Change beat type */}
+        </section>
+      </section>
+      <section className="bottom-panel">
+        <section>
           <button className="icon-btn" onClick={toggleTipsPanel}>
             <img src="/assets/svg/ui/help.svg" alt="Show or hide tips panel" />
           </button>
-        </div>
+        </section>
+        <section className="undo-redo-section">
+          <button className="icon-btn" title="undo">
+            <img src="/assets/svg/ui/undo.svg" alt="undo" />
+          </button>
+          <button className="icon-btn" title="redo">
+            <img src="/assets/svg/ui/undo.svg" alt="redo" className="flip-h" />
+          </button>
+        </section>
         <PlayContainer />
-        <div className="exporting-section">
+        <section className="exporting-section">
           <button className="icon-btn" onClick={showExportModal}>
             <img src="/assets/svg/ui/download.svg" alt="Download rhythm definition" />
           </button>
           <PrintingSystem />
-        </div>
-      </div>
+        </section>
+        <section className="copy-paste-section">
+          <button className="icon-btn" title="copy">
+            <img src="/assets/svg/ui/copy.svg" alt="copy" />
+          </button>
+          <button className="icon-btn" title="paste">
+            <img src="/assets/svg/ui/paste.svg" alt="paste" />
+          </button>
+        </section>
+      </section>
     </>
   )
 }
