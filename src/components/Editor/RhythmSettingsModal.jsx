2084cf854bfa5265ad57b7709f53263ccfc11d3e
Dominik Zawlocki <zawlockidominik@gmail.com>
2024-06-20 11:18:11 +0200

    WIP: Working on rearranging instruments in rhythms settings

--- src/components/Editor/RhythmSettingsModal.jsx
+++ src/components/Editor/RhythmSettingsModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./css/RhythmSettingsModal.css"
 import ModalWrapper from "./ModalWrapper";
@@ -96,22 +96,77 @@ function RhythmManagementSection() {
   })
   const definition = useSelector(store => store.rhythm.definition)
 
+  const [tempOrder, setTempOrder] = useState([])
+  console.log(tempOrder)
+
+  const [instrumentsToDelete, setInstrumentsToDelete] = useState([])
+  console.log("To delete: ", instrumentsToDelete)
+
+  useEffect(() => setTempOrder(Array.from({length: definition.length}, (_, i) => i)), [definition])
+  
+  const instrumentsManagement = {
+    moveInstrumentAtPosition: (oldTempIdx, newTempIdx) => {
+      if (oldTempIdx == newTempIdx) {
+        // We're moving to the same place
+        return
+      }
+
+      const instrumentIdx = tempOrder[oldTempIdx]
+
+      console.log("Moving instrument ", instrumentIdx, " at pos: ", newTempIdx)
+
+      const newTempOrder = [...tempOrder]
+
+      if (oldTempIdx < newTempIdx) {
+        for (let i = oldTempIdx; i < newTempIdx; i++) {
+          newTempOrder[i] = tempOrder[i+1]
+        }
+        newTempOrder[newTempIdx] = instrumentIdx
+      }else {
+        for (let i = newTempIdx; i < oldTempIdx; i++) {
+          newTempOrder[i+1] = tempOrder[i]
+        }
+        newTempOrder[newTempIdx] = instrumentIdx
+      }
+
+      setTempOrder(newTempOrder)
+    },
+    addNewInstrument: () => {
+      console.log("Adding new instrument")
+    },
+    deleteInstrument: (instrumentIdx) => {
+      console.log("Deleting instrument: ", instrumentIdx)
+      setInstrumentsToDelete([...instrumentsToDelete, instrumentIdx])
+    },
+    revertDeletion: (instrumentIdx) => {
+      console.log("Reverting deletion of: ", instrumentIdx)
+      setInstrumentsToDelete(instrumentsToDelete.filter(idx => idx != instrumentIdx))
+    }
+  }
+
   return (
     <section className="rhythm-management-section" style={ {backgroundColor: isOver ? "#777" : "#fff"} } ref={ drop }>
       {
-        definition.map((instrument, idx) => (
-          <DraggableInstrumentRow key={ idx } idx={ idx }/>
+        tempOrder.map((instrumentIdx, tempIdx) => (
+          <DraggableInstrumentRow 
+            key={ tempIdx } 
+            instrumentIdx={ instrumentIdx } 
+            tempIdx={ tempIdx }
+            toDelete={ instrumentsToDelete.includes(instrumentIdx) }
+            instrumentsManagement={ instrumentsManagement }
+          />
         ))
       }
     </section>
   )
 }
 
-function DraggableInstrumentRow({idx}) {
+function DraggableInstrumentRow({instrumentIdx, tempIdx, toDelete, instrumentsManagement}) {
   const [{isDragging}, drag] = useDrag({
     type: "instrument",
     item: {
-      instrumentIdx: idx,
+      instrumentIdx: instrumentIdx,
+      tempIdx: tempIdx,
     },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
@@ -119,18 +174,32 @@ function DraggableInstrumentRow({idx}) {
   })
 
   const [, drop] = useDrop({
+    accept: "instrument",
     hover: (item, monitor) => {
-      console.log(item, monitor)
+      console.log(tempIdx, item, monitor)
+      instrumentsManagement.moveInstrumentAtPosition(item.tempIdx, tempIdx)
+      item.tempIdx = tempIdx
     }
   })
 
+  function onDeleteBtnClicked() {
+    if (toDelete) {
+      instrumentsManagement.revertDeletion(instrumentIdx)
+    }else {
+      instrumentsManagement.deleteInstrument(instrumentIdx)
+    }
+  }
+  
   return (
-    <div className="draggable-instrument-row" ref={ drag }>
-      <button className="icon-btn" title="Delete this instrument">
-        <img src="/assets/svg/ui/trash.svg" alt="Delete"/>
-      </button>
-      <p style={ {fontWeight: isDragging ? "bold" : "normal"} }>Rhythm { idx + 1 }</p>
-      <div>%</div>
+    <div className="drop-wrapper" ref={drop}>
+      <div className="draggable-instrument-row" ref={ drag }>
+        <button className="icon-btn" title="Delete this instrument" onClick={onDeleteBtnClicked}>
+          <img src="/assets/svg/ui/trash.svg" alt="Delete"/>
+        </button>
+        <p style={ {fontWeight: isDragging ? "bold" : "normal"} }>Rhythm { instrumentIdx + 1 }</p>
+        <div>%</div>
+        { toDelete && <div className="to-delete">Will be deleted</div> }
+      </div>
     </div>
   )
 }
