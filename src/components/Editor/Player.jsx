6c32f8803ddd9c7437888e4d94c0d4387b80ef92
Dominik <zawlockidominik@gmail.com>
2026-04-04 19:56:53 +0200

    Refactor Player component and fix bugs with repeating playing

--- src/components/Editor/Player.jsx
+++ src/components/Editor/Player.jsx
@@ -1,128 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setCurrentBarIdx, stop } from "../../Redux/playerSlice"
 import { playNote } from "../../helpers/playing/playing"
-
-let localRhythmCopy = null
-let isPlayingRhythm = false
-let localRepeat = false
-
-
-function playRhythm(dispatch) {
-  isPlayingRhythm = true
-
-  // Delays are in millis
-  const singleBeatPartDuration = 60_000.0 / localRhythmCopy.tempo
-  const durationPerTypes = {
-    "single": singleBeatPartDuration,
-    "double": singleBeatPartDuration / 2,
-    "triplet": singleBeatPartDuration / 3,
-    "quartet": singleBeatPartDuration / 4,
-    "grace": singleBeatPartDuration / 4,
-  }
-
-  let instrumentsStillPlaying = localRhythmCopy.definition.length
-  
-  function onInstrumentFinished(replay, replayDelay) {
-    instrumentsStillPlaying--
-
-    if (instrumentsStillPlaying <= 0) {
-      if (replay) {
-        // Play again after delay, so that the previous rhythm fully finishes
-        setTimeout(() => playRhythm(dispatch), replayDelay)
-      } else {
-        // We don't repeat OR this is force stop (i.e. due to no notes found)
-        dispatch(stop())
-      }
-    }
-  }
-
-
-  // HACK: Delay playing all instruments by the duration of grace note, 
-  // in case there are any grace notes at the beggining (then the rhythms will be played a bit quicker)
-  const standardDelay = durationPerTypes["grace"]
-
-  function playInstrumentDelayed(nthInstrument, delay) {
-    playNoteDelayedAndContinue(nthInstrument, 0, 0, delay)
-  }
-
-  function playNoteDelayedAndContinue(nthInstrument, beatIdx, noteIdx, delay) {
-    if (!isPlayingRhythm) {
-        return
-    }
-  
-    const instrumentDefinition = localRhythmCopy.definition[nthInstrument]
-    const beatDef = instrumentDefinition[beatIdx]
-
-    if (beatDef === undefined) {
-      // No beats found - stopping without replay!
-      onInstrumentFinished(false)
-      return
-    }
-
-    const noteSymbol = beatDef.notes[noteIdx]
-
-    /**
-     * How long this note will be played
-     */
-    let duration = durationPerTypes[beatDef.type]
-  
-    // TODO
-    // if (beatPartType === "grace") {
-    //     const allNotesInPart = beatPart.querySelectorAll(".note")
-    //     const nthNote = Array.prototype.indexOf.call(allNotesInPart, noteBtn)
-  
-    //     if (nthNote === 0) {
-    //         // It's the first note of grace beat part. Let's play a bit earlier
-    //         delay -= durationPerTypes["grace"]
-    //     } else {
-    //         // It's second note in the grace beatPart - it should have a duration, as it was single note
-    //         duration = durationPerTypes["single"]
-    //     }
-    // }
-  
-    const highlightCurrentlyPlayedBeat = () => {
-      if (isPlayingRhythm) {
-        const barIdx = Math.floor(beatIdx / localRhythmCopy.beatsInBar)
-        dispatch(setCurrentBarIdx(barIdx))
-      }
-    }
-  
-    setTimeout(() => {
-      playNote(noteSymbol)
-      
-      highlightCurrentlyPlayedBeat()
-
-      const nextNoteIdx = noteIdx + 1
-
-      if (nextNoteIdx < beatDef.notes.length) {
-          // Queue playing the next note, delayed by the duration of currently played one
-          playNoteDelayedAndContinue(nthInstrument, beatIdx, nextNoteIdx, duration)
-      }else {
-          // End of the notes in beat. Let's play next beat instead, delayed by the duration of currently played note
-          const nextBeatIdx = beatIdx + 1
-          if (nextBeatIdx < localRhythmCopy.beatsCount) {
-              // There is next beat - let's play it!
-              playNoteDelayedAndContinue(nthInstrument, nextBeatIdx, 0, duration)
-          }else {
-              // End of the rhythm
-              const replayDelay = Math.max(duration - standardDelay, 0)
-              onInstrumentFinished(localRepeat, replayDelay)
-          }
-      }
-    }, delay)
-  }
-
-  localRhythmCopy.definition.forEach((_, nthInstrument) => {
-      playInstrumentDelayed(nthInstrument, standardDelay)
-  })
-}
-
-
-function onRhythmPlayingStopped(dispatch) {
-  isPlayingRhythm = false
-}
-
 
 export default function Player() {
   const rhythmData = useSelector(store => store.rhythm)
@@ -130,19 +9,108 @@ export default function Player() {
   const repeat = useSelector(store => store.player.repeat)
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    localRhythmCopy = rhythmData
-  }, [rhythmData])
+  const stateRef = useRef({
+    isPlaying: false,
+    timeoutIds: [],
+    rhythm: null,
+    repeat: false
+  })
 
-  useEffect(() => {
-    localRepeat = repeat
-  }, [repeat])
+  // Update refs when redux state changes
+  useEffect(() => { stateRef.current.rhythm = rhythmData }, [rhythmData])
+  useEffect(() => { stateRef.current.repeat = repeat }, [repeat])
+
+  const clearAllTimers = () => {
+    stateRef.current.timeoutIds.forEach(id => clearTimeout(id))
+    stateRef.current.timeoutIds = []
+  }
+
+  const playRhythm = (dispatch) => {
+    const { rhythm, repeat } = stateRef.current
+    if (!rhythm) return
+
+    const singleBeatPartDuration = 60_000.0 / rhythm.tempo
+    const durationPerTypes = {
+      "single": singleBeatPartDuration,
+      "double": singleBeatPartDuration / 2,
+      "triplet": singleBeatPartDuration / 3,
+      "quartet": singleBeatPartDuration / 4,
+      "grace": singleBeatPartDuration / 4,
+    }
+
+    let instrumentsStillPlaying = rhythm.definition.length
+    const standardDelay = durationPerTypes["grace"]
+
+    function onInstrumentFinished(shouldReplay, replayDelay) {
+      instrumentsStillPlaying--
+      
+      if (instrumentsStillPlaying <= 0) {
+        if (shouldReplay && stateRef.current.isPlaying) {
+          const tid = setTimeout(() => playRhythm(dispatch), replayDelay)
+          stateRef.current.timeoutIds.push(tid)
+        } else {
+          dispatch(stop())
+        }
+      }
+    }
+
+    function playNoteDelayedAndContinue(nthInstrument, beatIdx, noteIdx, delay) {
+      if (!stateRef.current.isPlaying) return
+
+      const instrumentDefinition = rhythm.definition[nthInstrument]
+      const beatDef = instrumentDefinition[beatIdx]
+
+      if (!beatDef) {
+        onInstrumentFinished(false, 0)
+        return
+      }
+
+      const tid = setTimeout(() => {
+        if (!stateRef.current.isPlaying) return
+
+        playNote(beatDef.notes[noteIdx])
+
+        // Highlight logic
+        const barIdx = Math.floor(beatIdx / rhythm.beatsInBar)
+        dispatch(setCurrentBarIdx(barIdx))
+
+        const nextNoteIdx = noteIdx + 1
+        let duration = durationPerTypes[beatDef.type]
+
+        if (nextNoteIdx < beatDef.notes.length) {
+          playNoteDelayedAndContinue(nthInstrument, beatIdx, nextNoteIdx, duration)
+        } else {
+          const nextBeatIdx = beatIdx + 1
+          if (nextBeatIdx < rhythm.beatsCount) {
+            playNoteDelayedAndContinue(nthInstrument, nextBeatIdx, 0, duration)
+          } else {
+            const replayDelay = Math.max(duration - standardDelay, 0)
+            onInstrumentFinished(stateRef.current.repeat, replayDelay)
+          }
+        }
+      }, delay)
+
+      stateRef.current.timeoutIds.push(tid)
+    }
+
+    rhythm.definition.forEach((_, nthInstrument) => {
+      playNoteDelayedAndContinue(nthInstrument, 0, 0, standardDelay)
+    })
+  }
 
   useEffect(() => {
     if (isPlaying) {
+      stateRef.current.isPlaying = true
       playRhythm(dispatch)
-    }else {
-      onRhythmPlayingStopped(dispatch)
+    } else {
+      stateRef.current.isPlaying = false
+      clearAllTimers()
+    }
+
+    // Cleanup function: stops everything if component unmounts
+    return () => {
+      stateRef.current.isPlaying = false
+      clearAllTimers()
     }
   }, [isPlaying])
 
